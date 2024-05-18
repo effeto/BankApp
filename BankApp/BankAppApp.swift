5d26d63c7090e5f80af4361d408a0514fc35a6fb
effeto <effectdopler@gmail.com>
2024-05-18 09:48:40 +0300

    Initial

---
+++ BankApp/BankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppApp.swift
+//  BankApp
+//
+//  Created by Демьян on 18.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct BankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
